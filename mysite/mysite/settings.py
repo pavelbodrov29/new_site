c39d7de6d40a38af470d0f1a10c477a03843cfc7
Pavel_B <pascha_7p@web.de>
2022-11-14 00:37:01 +0100

    testapp

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'ckeditor_uploader',
     'news.apps.NewsConfig',
     'captcha',
+    'testapp.apps.TestappConfig',
 
 ]
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,17 +18,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('news/', include('news.urls')),
     path('', include('news.urls')),
     path('captcha/', include('captcha.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('test/', include('testapp.urls')),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
