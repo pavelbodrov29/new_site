192fed1fa07416ab03cfc18de237e043f3a84047
Pavel_B <pascha_7p@web.de>
2022-11-14 01:42:28 +0100

    migrations

--- mysite/testapp/models.py
+++ mysite/testapp/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Genre(MPTTModel):
+class Rubric(MPTTModel):
     name = models.CharField(max_length=50, unique=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.name
 
     class MPTTMeta:
         order_insertion_by = ['name']
